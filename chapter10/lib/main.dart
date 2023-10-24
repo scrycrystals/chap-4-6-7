af2f89af08f1abcca6eedd12146b8dd55d411001
scrycrystals <scrycrystals2003@gmail.com>
2023-10-24 06:41:23 -0700

    Chapter10
    Chapter10 (classwork)

---
+++ chapter10/lib/main.dart
@@ -0,0 +1,18 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:chapter10/pages/home.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+ // This widget is the root of your application.
+ @override
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ debugShowCheckedModeBanner: false,
+ title: 'Starter Template',
+ theme: ThemeData(
+ primarySwatch: Colors.blue,
+ ),
+ home: Home(),
+ );
+ }
+}
